8332503bd8753ac93c833d5b3a9d010fdf3057bf
Andriy Hushchyn <hyshchun@gmail.com>
2021-09-14 20:35:58 +0300

    vvariables

---
+++ main.js
@@ -0,0 +1,6 @@
+let city;
+city = 'Kyiv';
+city = 'Lviv';
+let adress;
+adress = city;
+console.log(adress);
---
+++ main.ts
@@ -0,0 +1,8 @@
+let city: string;
+city = 'Kyiv'
+city = 'Lviv'
+
+let adress: string;
+adress = city;
+console.log(adress);
+
